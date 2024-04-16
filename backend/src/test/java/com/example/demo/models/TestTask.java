fa6147e0a84713a5f5b3aa0473f388dc4e467ffd
BaZZcheesy <107484617+BaZZcheesy@users.noreply.github.com>
2024-04-16 11:53:40 +0200

    Added first selenium test

--- backend/src/test/java/com/example/demo/models/TestTask.java
+++ backend/src/test/java/com/example/demo/models/TestTask.java
@@ -2,11 +2,65 @@ package com.example.demo.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.models.Task;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class TestTask {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        // WebDriverManager.chromedriver().setup();
+        // WebDriverManager.edgedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+        // per Selenium documentation Opera (02.03.2023) is not supported at the moment
+        // WebDriverManager.safaridriver().setup();
+    }
+
+    /**
+     * Initializes the driver each time a test is executed.
+     * 
+     * --> Uncomment the driver for the browser you want to use.
+     */
+    @BeforeEach
+    void setupTest() {
+        // ChromeOptions options = new ChromeOptions();
+    	// options.addArguments("--remote-allow-origins=*");
+        // driver = new ChromeDriver();
+        // driver = new EdgeDriver();
+        driver = new FirefoxDriver();
+        // per Selenium documentation (02.03.2023) Opera is not supported at the moment
+        // driver = new SafariDriver();
+    }
+
+    /**
+     * Shuts down the used browser session after every test.
+     */
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    // @Disabled("Not required after first successful test")
+    public void basicTest() {
+        driver.get("https://www.selenium.dev/");
+
+        String title = driver.getTitle();
+
+        assertEquals("Selenium", title);
+    }
 
     @Test
     public void testSetGetTaskDescription() {

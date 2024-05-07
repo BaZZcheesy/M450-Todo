b57b2ebc72bb7f61118af99b1b86cd6e8608b182
BaZZcheesy <107484617+BaZZcheesy@users.noreply.github.com>
2024-05-07 11:20:59 +0200

    Resolved Crossorigin security flaws

--- backend/src/main/java/com/example/controllers/TaskController.java
+++ backend/src/main/java/com/example/controllers/TaskController.java
@@ -16,37 +16,33 @@ import com.example.models.Task;
 import com.example.repository.TaskRepository;
 
 @RestController
+@CrossOrigin( origins = "http://localhost:3000/")
 @RequestMapping("/tasks")
 public class TaskController {
     @Autowired
 	private TaskRepository taskrepo;
 
-	@CrossOrigin
 	@GetMapping("/")
 	public List<Task> getTasks() {
 		return taskrepo.findAll();
 	}
 
-	@CrossOrigin
 	@GetMapping("/hello/{param}")
 	public String helloMessage(@PathVariable (value = "param") String param) {
 		return "Hello " + param;
 	}
 
-	@CrossOrigin
 	@GetMapping("/{param1}/{param2}")
 	public int calcTwoNumbers(@PathVariable (value = "param1") int param1, @PathVariable (value = "param2") int param2) {
 		return param1 * param2;
 	}
 
-	@CrossOrigin
 	@PostMapping("/add")
 	public ResponseEntity<Task> addTask(@RequestBody Task task) {
 		task = taskrepo.save(task);
 		return ResponseEntity.ok(task);
 	}
 
-	@CrossOrigin
 	@PostMapping("/delete")
 	public String delTask(@RequestBody Task taskToFind) {
 		Task task = taskrepo.findByTaskdescription(taskToFind.getTaskdescription());
